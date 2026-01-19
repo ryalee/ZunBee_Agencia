c5096da1751eb97b8a57036e3d7a7748a24650b0
Ryan <sodre100ryan@gmail.com>
2026-01-19 15:47:00 -0300

    Update BotaoContato.jsx

--- src/components/BotaoContato.jsx
+++ src/components/BotaoContato.jsx
@@ -8,7 +8,7 @@ export default function BotaoContato({ label = "Contato", className = "" }) {
     <>
       <button
         onClick={() => setIsOpen(true)}
-        className={`bg-main border-3 font-semibold w-full py-2 px-6 rounded-[15px] text-[16pt] hover:bg-main/90 transition cursor-pointer ${className}`}
+        className={`bg-main border-3 font-semibold w-[240px] py-2 px-6 rounded-[15px] text-[16pt] hover:bg-main/90 transition cursor-pointer ${className}`}
       >
         {label}
       </button>
